0a5a78ae918317b2f31ba6aa2a42ba2a03bb5d4e
Edward Saxton <eddsax@googlemail.com>
2010-09-14 18:16:54 +0100

    Add depenancies to question levels. Remove unneeded comments

--- lib/question_generator.py
+++ lib/question_generator.py
@@ -4,11 +4,12 @@ import random
 categories = []
 category_generators = {}
 category_descriptions = {}
+category_dependancy = {}
 
 
-
-def _add_category(name, description):
+def _add_category(name, description, dependancy=[]):
     categories.append(name)
+    category_dependancy[name] = dependancy
     def decorator(func):
         category_generators[name] = func
         category_descriptions[name] = description
@@ -38,7 +39,7 @@ def category_0():
     return "Which is {0} a) {1} or b) {2}?".format(goal, a, b,), "a" if answer else "b"
 
 
-@_add_category("0a", "Uneven decimal comparison")
+@_add_category("0a", "Uneven length decimal comparison", ["0"])
 def category_0():
     magnitude = random.choice([0.1, 1, 10, 100, 1000])
     a = random.randint(0, 1000)
@@ -52,27 +53,24 @@ def category_0():
     return "Which is {0} a) {1} or b) {2}?".format(goal, a, b,), "a" if answer else "b"
 
 
-# categories
-# 0 - decimal number comparisons
-# 0a - uneven decimal comparison
 # 1 - positive numbers
 # 1a - addition and subtraction
 # 1b - multiplication and devision
-# 1c - order and brackets
+# 1c - order and brackets -1a, 1b
 # 2 - negative numbers
-# 2a - addition and subtraction
-# 2b - multiplication and devisions
-# 2c - repeated negative multiplication
-# 2d - complex ordered questions with brackets and negatives
+# 2a - addition and subtraction - 1a
+# 2b - multiplication and devision -1b
+# 2c - repeated negative multiplication - 2b
+# 2d - complex ordered questions with brackets and negatives - 2a, 2c, 1c
 # 3 - si units
 # 3a - multiplication and devision by powers of 10
-# 3b - converting between si magnitude regardless of base
-# 3c - comparison of different magnitude si units
+# 3b - converting between si magnitude regardless of base - 3a
+# 3c - comparison of different magnitude si units - 3b, 0a
 # 4 - rounding
 # 4a - rounding to whole numbers
-# 4b - rounding to d.p.
-# 4c - rounding to s.f.
+# 4b - rounding to d.p. - 4a
+# 4c - rounding to s.f. -4b
 
 if __name__ == "__main__":
     for i in range(10):
-        print(generate_question("0"))
+        print(generate_question("0a"))
